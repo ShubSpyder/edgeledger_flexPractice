cf88742ee3eb5f32c0f10721395156f2f90cbed6
shub-shukla <shub.shukla@protonmail.com>
2020-06-07 11:23:51 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+//treansaprent nav after 150px
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
+
+//smooth scrolling
+$("#navbar a, .btn").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
+
+//map
+var map = new ol.Map({
+  target: "map",
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM(),
+    }),
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([72.89, 19.12]),
+    zoom: 14,
+  }),
+});
+
+//lonLat >> 72.8901311, 19.1234507
